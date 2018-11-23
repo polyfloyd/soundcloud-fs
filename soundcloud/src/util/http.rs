306e6fb73fd07581151fc9795e2b11bf7909d599
polyfloyd <floyd@polyfloyd.net>
2018-11-23 19:49:07 +0100

    Optimize RangeSeeker so it makes less HTTP requests

--- soundcloud/src/util/http.rs
+++ soundcloud/src/util/http.rs
@@ -1,13 +1,23 @@
 use reqwest::header::{self, HeaderValue};
 use reqwest::StatusCode;
 use std::io;
+use std::mem;
+
+enum RangeSeekerState {
+    NoResponse,
+    Response(reqwest::Response),
+    OutOfRange(u64),
+}
 
 pub struct RangeSeeker<'a> {
     req: reqwest::Request,
     client: &'a reqwest::Client,
 
-    res: Option<reqwest::Response>,
+    state: RangeSeekerState,
     current_offset: u64,
+
+    // The previous request scheme is used as an optimization for file size probes.
+    response_cache: Option<(reqwest::Response, u64)>,
 }
 
 impl<'a> RangeSeeker<'a> {
@@ -18,12 +28,13 @@ impl<'a> RangeSeeker<'a> {
         Ok(RangeSeeker {
             req,
             client,
-            res: None,
+            state: RangeSeekerState::NoResponse,
             current_offset: 0,
+            response_cache: None,
         })
     }
 
-    fn next_resp(&self) -> io::Result<reqwest::Response> {
+    fn next_resp(&mut self) -> io::Result<()> {
         let mut req = reqwest::Request::new(self.req.method().clone(), self.req.url().clone());
         req.headers_mut().insert(
             header::RANGE,
@@ -40,25 +51,56 @@ impl<'a> RangeSeeker<'a> {
         let res = self
             .client
             .execute(req)
-            .map_err(|err| io::Error::new(io::ErrorKind::Other, err))?
+            .map_err(|err| io::Error::new(io::ErrorKind::Other, err))?;
+
+        if res.status() == StatusCode::RANGE_NOT_SATISFIABLE {
+            if let Some(l) = content_length(&res) {
+                self.state = RangeSeekerState::OutOfRange(l);
+                return Ok(());
+            } else {
+                self.state = RangeSeekerState::NoResponse;
+                return Err(io::Error::new(
+                    io::ErrorKind::Other,
+                    "416 response has no Content-Length",
+                ));
+            }
+        }
+
+        let res = res
             .error_for_status()
             .map_err(|err| io::Error::new(io::ErrorKind::Other, err))?;
-        if res.status() != StatusCode::PARTIAL_CONTENT {
-            return Err(io::Error::new(
-                io::ErrorKind::Other,
-                "range request did not return Partial Content",
-            ));
+
+        if res.status() == StatusCode::PARTIAL_CONTENT {
+            self.state = RangeSeekerState::Response(res);
+            return Ok(());
         }
-        Ok(res)
+
+        self.state = RangeSeekerState::NoResponse;
+        Err(io::Error::new(
+            io::ErrorKind::Other,
+            format!(
+                "range request did not return Partial Content, got status {}",
+                res.status()
+            ),
+        ))
     }
 }
 
 impl<'a> io::Read for RangeSeeker<'a> {
     fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
-        if self.res.is_none() {
-            self.res = Some(self.next_resp()?);
+        // Drop any cached responses to avoid leaking connections.
+        self.response_cache = None;
+
+        if let RangeSeekerState::NoResponse = self.state {
+            self.next_resp()?;
         }
-        let res = self.res.as_mut().unwrap();
+        let res = match self.state {
+            RangeSeekerState::Response(ref mut res) => res,
+            RangeSeekerState::OutOfRange(_) => {
+                return Ok(0);
+            }
+            _ => unreachable!(),
+        };
 
         let mut nread = 0;
         let mut n = 1;
@@ -73,22 +115,23 @@ impl<'a> io::Read for RangeSeeker<'a> {
 
 impl<'a> io::Seek for RangeSeeker<'a> {
     fn seek(&mut self, pos: io::SeekFrom) -> io::Result<u64> {
-        if self.res.is_none() {
-            self.res = Some(self.next_resp()?);
+        if let RangeSeekerState::NoResponse = self.state {
+            self.next_resp()?;
         }
-        let res = self.res.as_mut().unwrap();
+        let content_length = match self.state {
+            RangeSeekerState::Response(ref mut res) => {
+                let content_length = content_length(res).ok_or_else(|| {
+                    io::Error::new(
+                        io::ErrorKind::Other,
+                        "can not seek relative to end, unknown Content-Length",
+                    )
+                })?;
+                content_length as i64
+            }
+            RangeSeekerState::OutOfRange(len) => len as i64,
+            _ => unreachable!(),
+        };
 
-        let content_length: i64 = res
-            .headers()
-            .get(header::CONTENT_LENGTH)
-            .and_then(|ct_len| ct_len.to_str().ok())
-            .and_then(|ct_len| ct_len.parse().ok())
-            .ok_or_else(|| {
-                io::Error::new(
-                    io::ErrorKind::Other,
-                    "can not seek relative to end, unknown Content-Length",
-                )
-            })?;
         let offset: i64 = match pos {
             io::SeekFrom::Start(offset) => offset as i64,
             io::SeekFrom::End(offset) => content_length + offset,
@@ -104,7 +147,43 @@ impl<'a> io::Seek for RangeSeeker<'a> {
             ));
         }
         let abs_offset = offset as u64;
+
+        let mut new_state = RangeSeekerState::NoResponse;
+
+        // io::SeekFrom::End(0) should seek to the end of the stream and causes no more bytes to be
+        // read after this. We add this special case to avoid a needless HTTP request to an empty
+        // body.
+        if pos == io::SeekFrom::End(0) {
+            new_state = RangeSeekerState::OutOfRange(content_length as u64);
+        }
+
+        if self.current_offset != abs_offset {
+            // Get the previous state. This also rewrites the old state to new state so the next
+            // operation will trigger a HTTP request if needed.
+            mem::swap(&mut self.state, &mut new_state);
+            let previous_offset = self.current_offset;
+            let previous_response = match new_state {
+                RangeSeekerState::Response(res) => Some(res),
+                _ => None,
+            };
+            // If we have a cached response that has the same absolute offset as desired, reuse it.
+            if self.response_cache.as_ref().map(|(_, o)| *o) == Some(abs_offset) {
+                let (cached_response, _) = self.response_cache.take().unwrap();
+                self.state = RangeSeekerState::Response(cached_response);
+            }
+            // Cache the old response so we can reuse it later.
+            if let Some(res) = previous_response {
+                self.response_cache = Some((res, previous_offset));
+            }
+        }
         self.current_offset = abs_offset;
         Ok(abs_offset)
     }
+}
+
+fn content_length(res: &reqwest::Response) -> Option<u64> {
+    res.headers()
+        .get(header::CONTENT_LENGTH)
+        .and_then(|ct_len| ct_len.to_str().ok())
+        .and_then(|ct_len| ct_len.parse().ok())
 }
