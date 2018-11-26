21100a24f8bf3ebf2fbf379ee08d40fc323672f9
polyfloyd <floyd@polyfloyd.net>
2018-11-26 21:38:37 +0100

    Add a CLI interface

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 extern crate chrono;
+extern crate clap;
 extern crate env_logger;
 extern crate failure;
 #[macro_use]
@@ -17,30 +18,61 @@ mod mapping;
 
 use self::filesystem::*;
 use self::mapping::*;
-use std::env;
-use std::path::Path;
 use std::process;
 
 fn main() {
     env_logger::init();
 
-    let username = env::var("SC_USERNAME").unwrap();
-    let password = env::var("SC_PASSWORD").ok();
+    let cli = clap::App::new("SoundCloud FS")
+        .version("0.1.0")
+        .author("polyfloyd <floyd@polyfloyd.net>")
+        .about("A FUSE driver for SoundCloud audio")
+        .arg(
+            clap::Arg::with_name("path")
+                .short("p")
+                .long("path")
+                .takes_value(true)
+                .required(true)
+                .help("Sets the target directory of the mount"),
+        ).arg(
+            clap::Arg::with_name("user")
+                .short("u")
+                .long("user")
+                .takes_value(true)
+                .required(true)
+                .help("Sets the user to create directory and file entries for"),
+        ).arg(
+            clap::Arg::with_name("login")
+                .long("login")
+                .value_name("username:password")
+                .takes_value(true)
+                .validator(|s| match s.splitn(2, ":").count() {
+                    2 => Ok(()),
+                    c => Err(format!("bad credential format, split on : yields {} strings", c)),
+                }).help("Logs in using a username and password instead of accessing the API anonymously"),
+        ).get_matches();
+
+    let login = cli.value_of("login").and_then(|s| {
+        let mut i = s.splitn(2, ":");
+        let u = i.next().unwrap();
+        i.next().map(|p| (u, p))
+    });
+
     let client_cache_path = "/tmp/sc-test-token";
 
-    let sc_client_rs = match password {
+    let sc_client_rs = match login {
         None => {
             info!("creating anonymous client");
             soundcloud::Client::anonymous()
         }
-        Some(pw) => soundcloud::Client::from_cache(client_cache_path)
+        Some((username, password)) => soundcloud::Client::from_cache(client_cache_path)
             .map(|v| {
                 info!("loaded client from cache");
                 v
             }).or_else(|err| {
                 error!("{}", err);
                 info!("logging in as {}", username);
-                soundcloud::Client::login(&username, pw)
+                soundcloud::Client::login(&username, password)
             }),
     };
 
@@ -51,18 +83,22 @@ fn main() {
             process::exit(1);
         }
     };
-    if let Err(err) = sc_client.cache_to(client_cache_path) {
-        error!(
+
+    match sc_client.cache_to(client_cache_path) {
+        Ok(_) => (),
+        Err(soundcloud::Error::NoToken) => (),
+        Err(err) => error!(
             "could not cache SoundCloud client to {}: {}",
             client_cache_path, err
-        );
-    }
+        ),
+    };
 
+    let username = cli.value_of("user").unwrap();
     let user = soundcloud::User::by_name(&sc_client, &username).unwrap();
     let fs = FS::new(NodeCache::new(Entry::User {
         user,
         recurse: true,
     }));
-    let path = Path::new("/home/polyfloyd/sc-test");
+    let path = cli.value_of("path").unwrap();
     fuse::mount(fs, &path, &[]).unwrap();
 }
