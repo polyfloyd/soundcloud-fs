26cacefd2bd2987c6c6cc48f7efe5f2835668d17
polyfloyd <floyd@polyfloyd.net>
2018-12-07 18:16:10 +0100

    Add unit tests for RangeSeeker

--- soundcloud/src/util/http.rs
+++ soundcloud/src/util/http.rs
@@ -6,15 +6,17 @@ use std::mem;
 enum RangeSeekerState {
     NoResponse,
     Response(reqwest::Response),
-    OutOfRange(u64),
+    OutOfRange,
 }
 
 pub struct RangeSeeker<'a> {
-    req: reqwest::Request,
     client: &'a reqwest::Client,
+    req: reqwest::Request,
+    num_requests: u64,
 
     state: RangeSeekerState,
     current_offset: u64,
+    content_length: Option<u64>,
 
     // The previous request scheme is used as an optimization for file size probes.
     response_cache: Option<(reqwest::Response, u64)>,
@@ -26,10 +28,12 @@ impl<'a> RangeSeeker<'a> {
         req: reqwest::Request,
     ) -> Result<RangeSeeker, reqwest::Error> {
         Ok(RangeSeeker {
-            req,
             client,
+            req,
+            num_requests: 0,
             state: RangeSeekerState::NoResponse,
             current_offset: 0,
+            content_length: None,
             response_cache: None,
         })
     }
@@ -48,29 +52,31 @@ impl<'a> RangeSeeker<'a> {
             req.url(),
             self.current_offset
         );
+        self.num_requests += 1;
         let res = self
             .client
             .execute(req)
             .map_err(|err| io::Error::new(io::ErrorKind::Other, err))?;
 
         if res.status() == StatusCode::RANGE_NOT_SATISFIABLE {
-            if let Some(l) = content_length(&res) {
-                self.state = RangeSeekerState::OutOfRange(l);
-                return Ok(());
-            } else {
-                self.state = RangeSeekerState::NoResponse;
-                return Err(io::Error::new(
-                    io::ErrorKind::Other,
-                    "416 response has no Content-Length",
-                ));
-            }
+            self.state = RangeSeekerState::OutOfRange;
+            return Ok(());
         }
 
         let res = res
             .error_for_status()
             .map_err(|err| io::Error::new(io::ErrorKind::Other, err))?;
 
-        if res.status() == StatusCode::PARTIAL_CONTENT {
+        if (self.current_offset == 0 && res.status() == StatusCode::OK)
+            || res.status() == StatusCode::PARTIAL_CONTENT
+        {
+            let clen = content_length(&res).ok_or_else(|| {
+                io::Error::new(
+                    io::ErrorKind::Other,
+                    "response did not include Content-Length",
+                )
+            })?;
+            self.content_length = Some(self.current_offset + clen);
             self.state = RangeSeekerState::Response(res);
             return Ok(());
         }
@@ -91,12 +97,18 @@ impl<'a> io::Read for RangeSeeker<'a> {
         // Drop any cached responses to avoid leaking connections.
         self.response_cache = None;
 
+        if let Some(l) = self.content_length {
+            if self.current_offset >= l {
+                return Ok(0);
+            }
+        }
+
         if let RangeSeekerState::NoResponse = self.state {
             self.next_resp()?;
         }
         let res = match self.state {
             RangeSeekerState::Response(ref mut res) => res,
-            RangeSeekerState::OutOfRange(_) => {
+            RangeSeekerState::OutOfRange => {
                 return Ok(0);
             }
             _ => unreachable!(),
@@ -115,47 +127,27 @@ impl<'a> io::Read for RangeSeeker<'a> {
 
 impl<'a> io::Seek for RangeSeeker<'a> {
     fn seek(&mut self, pos: io::SeekFrom) -> io::Result<u64> {
-        if let RangeSeekerState::NoResponse = self.state {
-            self.next_resp()?;
-        }
-        let content_length = match self.state {
-            RangeSeekerState::Response(ref mut res) => {
-                let content_length = content_length(res).ok_or_else(|| {
-                    io::Error::new(
-                        io::ErrorKind::Other,
-                        "can not seek relative to end, unknown Content-Length",
-                    )
-                })?;
-                content_length as i64
-            }
-            RangeSeekerState::OutOfRange(len) => len as i64,
-            _ => unreachable!(),
+        let abs_offset = match pos {
+            io::SeekFrom::Start(offset) => offset,
+            io::SeekFrom::End(offset) => {
+                if self.content_length.is_none() {
+                    self.next_resp()?;
+                }
+                valid_offset(self.content_length.unwrap() as i64 + offset)?
+            },
+            io::SeekFrom::Current(offset) => {
+                valid_offset(self.current_offset as i64 + offset)?
+            },
         };
 
-        let offset: i64 = match pos {
-            io::SeekFrom::Start(offset) => offset as i64,
-            io::SeekFrom::End(offset) => content_length + offset,
-            io::SeekFrom::Current(offset) => self.current_offset as i64 + offset,
+        let mut new_state = if pos == io::SeekFrom::End(0) {
+            // io::SeekFrom::End(0) should seek to the end of the stream and causes no more bytes
+            // to be read after this. We add this special case to avoid a needless HTTP request to
+            // an empty body.
+            RangeSeekerState::OutOfRange
+        } else {
+            RangeSeekerState::NoResponse
         };
-        if offset < 0 || content_length < offset {
-            return Err(io::Error::new(
-                io::ErrorKind::Other,
-                format!(
-                    "http::RangeSeeker: seek position {:?} outside range 0..{}",
-                    pos, content_length
-                ),
-            ));
-        }
-        let abs_offset = offset as u64;
-
-        let mut new_state = RangeSeekerState::NoResponse;
-
-        // io::SeekFrom::End(0) should seek to the end of the stream and causes no more bytes to be
-        // read after this. We add this special case to avoid a needless HTTP request to an empty
-        // body.
-        if pos == io::SeekFrom::End(0) {
-            new_state = RangeSeekerState::OutOfRange(content_length as u64);
-        }
 
         if self.current_offset != abs_offset {
             // Get the previous state. This also rewrites the old state to new state so the next
@@ -186,4 +178,137 @@ fn content_length(res: &reqwest::Response) -> Option<u64> {
         .get(header::CONTENT_LENGTH)
         .and_then(|ct_len| ct_len.to_str().ok())
         .and_then(|ct_len| ct_len.parse().ok())
+}
+
+fn valid_offset(offset: i64) -> Result<u64, io::Error> {
+    if offset < 0 {
+        return Err(io::Error::new(
+            io::ErrorKind::Other,
+            format!("http::RangeSeeker: can not seek to {}", offset),
+        ));
+    }
+    Ok(offset as u64)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::io::{Read, Seek};
+
+    fn test_request(size: usize) -> reqwest::Request {
+        // Ideally, we are not dependant on an external server for unit tests...
+        let mut req = reqwest::Request::new(
+            reqwest::Method::GET,
+            format!("https://httpbin.org/range/{}", size)
+                .parse()
+                .unwrap(),
+        );
+        req.headers_mut().insert(
+            header::ACCEPT,
+            HeaderValue::from_static("application/octet-stream"),
+        );
+        req
+    }
+
+    fn test_request_resp(start: usize, end: usize) -> Vec<u8> {
+        (97..=122)
+            .into_iter()
+            .cycle()
+            .skip(start)
+            .take(end - start)
+            .collect()
+    }
+
+    #[test]
+    fn test_read_all() {
+        const SIZE: usize = 8192;
+        let client = reqwest::Client::new();
+        let req = test_request(SIZE);
+
+        let mut f = RangeSeeker::new(&client, req).unwrap();
+
+        let mut buf = Vec::new();
+        f.read_to_end(&mut buf).unwrap();
+        assert_eq!(test_request_resp(0, SIZE), buf);
+
+        assert_eq!(1, f.num_requests);
+    }
+
+    #[test]
+    fn test_read_partial() {
+        const SIZE: usize = 8192;
+        let client = reqwest::Client::new();
+        let req = test_request(SIZE);
+
+        let mut f = RangeSeeker::new(&client, req).unwrap();
+
+        let new_pos = f.seek(io::SeekFrom::Start(4000)).unwrap();
+        assert_eq!(4000, new_pos);
+
+        let mut buf = Vec::new();
+        f.read_to_end(&mut buf).unwrap();
+        assert_eq!(test_request_resp(4000, SIZE), buf);
+
+        assert_eq!(1, f.num_requests);
+    }
+
+    #[test]
+    fn test_seek_to_end() {
+        const SIZE: usize = 8192;
+        let client = reqwest::Client::new();
+        let req = test_request(SIZE);
+
+        let mut f = RangeSeeker::new(&client, req).unwrap();
+
+        let new_pos = f.seek(io::SeekFrom::End(0)).unwrap();
+        assert_eq!(SIZE as u64, new_pos);
+
+        let mut buf = Vec::new();
+        f.read_to_end(&mut buf).unwrap();
+        assert!(buf.is_empty());
+
+        assert_eq!(1, f.num_requests);
+    }
+
+    #[test]
+    fn test_probe_size() {
+        const SIZE: usize = 8192;
+        let client = reqwest::Client::new();
+        let req = test_request(SIZE);
+
+        let mut f = RangeSeeker::new(&client, req).unwrap();
+
+        let new_pos = f.seek(io::SeekFrom::End(0)).unwrap();
+        assert_eq!(SIZE as u64, new_pos);
+
+        let new_pos = f.seek(io::SeekFrom::Start(0)).unwrap();
+        assert_eq!(0, new_pos);
+
+        let mut buf = Vec::new();
+        f.read_to_end(&mut buf).unwrap();
+        assert_eq!(test_request_resp(0, SIZE), buf);
+
+        assert_eq!(1, f.num_requests);
+    }
+
+    #[test]
+    fn test_read_after_seek() {
+        const SIZE: usize = 8192;
+        let client = reqwest::Client::new();
+        let req = test_request(SIZE);
+
+        let mut f = RangeSeeker::new(&client, req).unwrap();
+
+        let new_pos = f.seek(io::SeekFrom::End(-100)).unwrap();
+        assert_eq!(SIZE as u64 - 100, new_pos);
+        let mut buf = [0; 50];
+        f.read_exact(&mut buf).unwrap();
+
+        let new_pos = f.seek(io::SeekFrom::End(-10)).unwrap();
+        assert_eq!(SIZE as u64 - 10, new_pos);
+
+        let mut buf = Vec::new();
+        f.read_to_end(&mut buf).unwrap();
+        assert_eq!(test_request_resp(SIZE - 10, SIZE), buf);
+    }
 }
